cba0382b959b5d7fa4cadfac154edc50b88aeaf3
Dhyder <victor.wairiguini@student.moringaschool.com>
2022-01-10 19:44:11 +0300

    imported models signupform,businessform,login,authenticate,httpresponseredirect,user

--- neighborhood/views.py
+++ neighborhood/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import SignupForm, BusinessForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
