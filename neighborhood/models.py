f97f06cdaad9a85c2bd1c15a1892dd1d0ba4299d
Dhyder <victor.wairiguini@student.moringaschool.com>
2022-01-10 19:38:33 +0300

    created class business

--- neighborhood/models.py
+++ neighborhood/models.py
@@ -49,3 +49,14 @@ class Profile(models.Model):
     @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
+
+
+class Business(models.Model):
+    name = models.CharField(max_length=120)
+    email = models.EmailField(max_length=254)
+    description = models.TextField(blank=True)
+    neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.CASCADE, related_name='business')
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='owner')
+
+    def __str__(self):
+        return f'{self.name} Business'
