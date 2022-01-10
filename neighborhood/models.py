c37c5c92c30b453fb0636ba72d55d9f9c4de67bd
Dhyder <victor.wairiguini@student.moringaschool.com>
2022-01-10 19:37:09 +0300

    created @receiver method

--- neighborhood/models.py
+++ neighborhood/models.py
@@ -39,3 +39,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} profile'
+
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
