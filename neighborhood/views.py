7014d633c41172fd5741c6cfdec94cac26641857
Dhyder <victor.wairiguini@student.moringaschool.com>
2022-01-10 19:45:01 +0300

    created def index for index.html

--- neighborhood/views.py
+++ neighborhood/views.py
@@ -5,3 +5,9 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
+
+@login_required(login_url='login')
+def index(request):
+    return render(request, 'index.html')
