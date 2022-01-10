b024466e99d34eb2dc3745db79900fd3204a9f5f
Dhyder <victor.wairiguini@student.moringaschool.com>
2022-01-10 19:58:26 +0300

    created postform

--- neighborhood/forms.py
+++ neighborhood/forms.py
@@ -39,3 +39,8 @@ class BusinessForm(forms.ModelForm):
     class Meta:
         model = Business
         exclude = ('user', 'neighbourhood')
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ('user', 'hood')
