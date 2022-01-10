a847eeaeadf935118678eaf304ff2d535d96fb47
Dhyder <victor.wairiguini@student.moringaschool.com>
2022-01-10 19:31:17 +0300

    created neighbourhood models

--- neighborhood/models.py
+++ neighborhood/models.py
@@ -6,3 +6,16 @@ from django.db.models.signals import post_save
 
 
 # Create your models here.
+
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=60)
+    admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
+    hood_logo = models.ImageField(upload_to='images/', default='cooker.png')
+    description = models.TextField()
+    health_tell = models.IntegerField(null=True, blank=True)
+    police_number = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name} hood'
+
