798ea17f4fd390a8196ebac16c66084a4bef3a33
Dhyder <victor.wairiguini@student.moringaschool.com>
2022-01-10 19:41:53 +0300

    registered models

--- neighborhood/admin.py
+++ neighborhood/admin.py
@@ -3,3 +3,8 @@ from .models import Profile, NeighbourHood, Business, Post
 
 
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(NeighbourHood)
+admin.site.register(Business)
+admin.site.register(Post)
