01759f750ed4d66b7b083aceadfd041d12cd3ebe
Dhyder <victor.wairiguini@student.moringaschool.com>
2022-01-11 02:12:24 +0300

    hoodlogo

--- neighborhood/models.py
+++ neighborhood/models.py
@@ -11,8 +11,8 @@ class NeighbourHood(models.Model):
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=60)
     admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
-    hood_logo = models.ImageField(upload_to='images/', default='cooker.png')
     description = models.TextField()
+    hood_logo = ImageField(blank=True, manual_crop='1280x720')
     health_tell = models.IntegerField(null=True, blank=True)
     police_number = models.IntegerField(null=True, blank=True)
 
