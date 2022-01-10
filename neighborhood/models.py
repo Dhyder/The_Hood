51a39ba87059812ad12d19fb6cb849f7c6dd85c1
Dhyder <victor.wairiguini@student.moringaschool.com>
2022-01-10 19:28:03 +0300

    imported imagefield grom uploadcare &&auth models

--- neighborhood/models.py
+++ neighborhood/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from pyuploadcare.dj.models import ImageField
+from django.dispatch import receiver
 
 # Create your models here.
