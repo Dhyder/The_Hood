38010fa75cf9d739b6c8fc083747485dd9844350
Dhyder <victor.wairiguini@student.moringaschool.com>
2022-01-10 19:33:14 +0300

    created def delete & create neighbourhood

--- neighborhood/models.py
+++ neighborhood/models.py
@@ -19,3 +19,8 @@ class NeighbourHood(models.Model):
     def __str__(self):
         return f'{self.name} hood'
 
+    def create_neighborhood(self):
+        self.save()
+
+    def delete_neighborhood(self):
+        self.delete()
