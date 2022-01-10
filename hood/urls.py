e05a879fe0813ce1e10e53ef38ff63e23783a3a7
Dhyder <victor.wairiguini@student.moringaschool.com>
2022-01-10 20:27:06 +0300

    url patterns

--- hood/urls.py
+++ hood/urls.py
@@ -20,4 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('neighborhood.urls'))
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
