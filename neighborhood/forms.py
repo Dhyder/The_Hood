684164656ca72933005404bfc2e123aace2c15f1
Dhyder <victor.wairiguini@student.moringaschool.com>
2022-01-10 19:55:18 +0300

    created class signupform

--- neighborhood/forms.py
+++ neighborhood/forms.py
@@ -3,3 +3,20 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile, NeighbourHood, Business, Post
 from pyuploadcare.dj.forms import ImageField
+
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text=None)
+    error_messages = {
+    'password_mismatch': ("The two password fields didn't match."),
+}
+    password1 = forms.CharField(
+    label=("Password"),
+    strip=False,
+    widget=forms.PasswordInput,
+)
+    password2 = forms.CharField(
+    label=("Password confirmation"),
+    widget=forms.PasswordInput,
+    strip=False,
+)
